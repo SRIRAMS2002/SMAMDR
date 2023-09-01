ed25bf433c17115aa0961895baf932b46a402185
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-09-01 22:40:01 +0530

    demo 2

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Example() {
                     </div>
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Deactivate account
+                        sriram
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
